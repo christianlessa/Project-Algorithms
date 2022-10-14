5e2cc30319e90547f0d0715d22c04aa5803eacba
Christian Lessa <christianclessa@gmail.com>
2022-10-14 18:46:23 -0300

    function is_anagram completed

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    
+    if (first_string  == "" or second_string == ""):
+        return False
+    
+    first = list(first_string.lower())
+    second = list(second_string.lower())
+
+    for index in first:
+        if (first.count(index) != second.count(index)):
+            return False
+
+    return True
+
+    
