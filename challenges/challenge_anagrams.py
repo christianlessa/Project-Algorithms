518c9c09e0e585c78d4792fea0302fb00c340b8c
Christian Lessa <christianclessa@gmail.com>
2022-10-18 09:42:42 -0300

    refactoring function is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,14 @@
 def is_anagram(first_string, second_string):
-    if (first_string == "" or second_string == ""):
+    if len(first_string) != len(second_string):
         return False
+
     first = list(first_string.lower())
     second = list(second_string.lower())
 
     for index in first:
-        if (first.count(index) != second.count(index)):
+        if index not in second:
             return False
+        else:
+            second.remove(index)
 
     return True
